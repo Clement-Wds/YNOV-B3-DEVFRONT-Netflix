d899c36d7a7ba6e9ea43f806980490f001c85acf
Clément Wds <clementwalshdeserrant@outlook.com>
2022-03-29 11:37:52 +0200

    index page

---
+++ src/pages/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Index = () => {
+  return (
+    <div className="login-page">
+        <div className="hero-main">
+            <div className="hero-content">
+                <h1>Films, séries TV et bien plus en illimité.</h1>
+                <h4>Ou que vous soyez. Annulez à tout moment.</h4>
+                <p>Prêt à regarder Netflix ? Saisissez votre adresse e-mail pour vous abonner ou réactiver votre abonnement.</p>
+                <div className="input-group">
+                    <input type="text" className="input" placeholder="Adresse e-mail" />
+                    <button type="button" className="button button-primary">Commencer</button>
+                </div>
+            </div>
+        </div>
+    </div>
+  );
+}
+
+export default Index;
