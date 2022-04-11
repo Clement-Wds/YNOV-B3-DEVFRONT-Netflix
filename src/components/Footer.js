652ef9bcfdd9ce1163a45ccbdc6dc884c9162949
Clément Wds <clementwalshdeserrant@outlook.com>
2022-04-11 21:42:40 +0200

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,6 +3,60 @@ import React from 'react';
 function Footer() {
   return(
     <div className="footer-main">
+        <div className="footer-row">
+          <div className="footer-col">
+            <ul>
+              <li>
+                <a href="">Audiodescription</a>
+              </li>
+              <li>
+                <a href="">Relations Investisseurs</a>
+              </li>
+              <li>
+                <a href="">Informations légales</a>
+              </li>
+            </ul>
+          </div>
+          <div className="footer-col">
+            <ul>
+              <li>
+                <a href="">Centre d'aide</a>
+              </li>
+              <li>
+                <a href="">Recrutement</a>
+              </li>
+              <li>
+                <a href="">Préférences de cookies</a>
+              </li>
+            </ul>
+          </div>
+          <div className="footer-col">
+            <ul>
+              <li>
+                <a href="">Cartes cadeaux</a>
+              </li>
+              <li>
+                <a href="">Conditions d'utilisation</a>
+              </li>
+              <li>
+                <a href="">Mentions légales</a>
+              </li>
+            </ul>
+          </div>
+          <div className="footer-col">
+            <ul>
+              <li>
+                <a href="">Presse</a>
+              </li>
+              <li>
+                <a href="">Confidentialité</a>
+              </li>
+              <li>
+                <a href="">Nous contacter</a>
+              </li>
+            </ul>
+          </div>
+        </div>
         <p>Copyright © 2022 - Netflix Clone by Clément Wds</p>
     </div>
   );
