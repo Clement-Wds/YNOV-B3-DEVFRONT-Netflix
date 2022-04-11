0b1f6b879c2b47e940978ae4a43e33021b367227
Clément Wds <clementwalshdeserrant@outlook.com>
2022-04-11 23:11:21 +0200

    white list

--- src/pages/white_list/index.js
+++ src/pages/white_list/index.js
@@ -1,68 +1,44 @@
-import React, {useEffect, useState} from 'react';
-import axios from "/node_modules/axios";
+import React, { useEffect, useState } from "react";
 
-import movieService from "/src/services/movie.service";
+const white_list = () => {
 
-const Row = ({name, path}) => {
-
-    const fetchUrl = `https://api.themoviedb.org/3/movie/${path}?api_key=b130d867055ff0c09fffec4729292241`;
     const [movies, setMovies] = useState([]);
 
-    const addToList = (element) => {
-        let movieToInsert = {
-          id: element.id,
-          title: element.title,
-          image: element.poster_path
-        };
-    
+    const deleteToList = (element) => {
         const whiteList = [];
-    
-        if(localStorage.getItem("whiteList")) {
-          const localStorageWhiteListe = JSON.parse(localStorage.getItem("whiteList"));
-          localStorageWhiteListe.forEach((movie) => {
+        
+        const localStorageWhiteListe = JSON.parse(localStorage.getItem("whiteList"));
+        localStorageWhiteListe.forEach((movie) => {
             whiteList.push(movie);
-          });
+        });
 
-          const indexOfExistingMovie = whiteList.findIndex((el) => el.id === element.id);
+        const indexOfExistingMovie = whiteList.findIndex((el) => el.id === element.id);
 
-          if (indexOfExistingMovie !== -1) {
+        if (indexOfExistingMovie !== -1) {
+            whiteList.splice(indexOfExistingMovie, 1);
             localStorage.setItem("whiteList", JSON.stringify(whiteList));
-          }
-          else {
-            whiteList.push(movieToInsert);
-            localStorage.setItem("whiteList", JSON.stringify(whiteList));
-          }
+            location.reload();
         }
-        else{
-          whiteList.push(movieToInsert);
-          localStorage.setItem("whiteList", JSON.stringify(whiteList));
-        };
     }
 
     useEffect(() => {
-        async function fetchData(){
-            const request = await axios.get(fetchUrl);
-            setMovies(request.data.results);
-            return request;
-        }
-        fetchData();
-          
-    },[]);
-    
-    console.log(movies);
+        setMovies(JSON.parse(localStorage.getItem("whiteList")) || []);
+    }, []);
 
-    return(
-        <div className="row">
-            <h2 className="row-title">{name}</h2>
+  return (
+    <div className="row">
+        <h2 className="row-title">Favoris</h2>
 
-            <div className="card">
+        <div className="card">
 
+            {movies ? (
+                <>
                 {movies.map((movie) => (
-                            
+                        
                     <div className="card-body">
-
-                        <img key={movie.id} src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} alt={movie.original_title} />
-
+    
+                        <img key={movie.id} src={`https://image.tmdb.org/t/p/original/${movie.image}`} alt={movie.title} />
+    
                         <div className="card-button-group">
                             <button type="button" className="card-button">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" viewBox="0 0 16 16">
@@ -70,30 +46,34 @@ const Row = ({name, path}) => {
                                     <path d="M6.271 5.055a.5.5 0 0 1 .52.038l3.5 2.5a.5.5 0 0 1 0 .814l-3.5 2.5A.5.5 0 0 1 6 10.5v-5a.5.5 0 0 1 .271-.445z"/>
                                 </svg>
                             </button>
-
+        
                             <button type="button" className="card-button">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" viewBox="0 0 16 16">
                                     <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
                                     <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
                                 </svg>
                             </button>
-
-                            <button type="button" className="card-button" onClick={() => addToList(movie)}>
+        
+                            <button type="button" className="card-button" onClick={() => deleteToList(movie)}>
                                 <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" viewBox="0 0 16 16">
                                     <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-                                    <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+                                    <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
                                 </svg>
                             </button>
                         </div>
-
+    
                     </div>
-
+    
                 ))}
-
-            </div>
+                </>
+            ) : (
+                <p>Votre liste de favoris est vide</p>
+            )}
 
         </div>
-    )
+
+    </div>
+  )
 }
 
-export default Row
+export default white_list
